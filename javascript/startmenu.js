be7ab554f6a9af8c395b7def59716feaa1e119a1
Jarmo <36207725+ja-rmo@users.noreply.github.com>
2022-01-24 16:21:02 +0100

    bugfixes

--- javascript/startmenu.js
+++ javascript/startmenu.js
@@ -1,7 +1,8 @@
 window.onload = function() {
     const theetuinKnop = document.getElementById("js--theetuinKnop");
     const bodegaKnop = document.getElementById("js--bodegaKnop");
-    const terugKnop = document.getElementById("js--terugKnop");
+    const terugKnop1 = document.getElementById("js--terugKnop1");
+    const terugKnop2 = document.getElementById("js--terugKnop2");
     const theetuin = document.getElementById("js--theetuin");
     const bodega = document.getElementById("js--bodega");
     const home = document.getElementById("js--home");
@@ -14,7 +15,12 @@ window.onload = function() {
         home.style.display = "none";
         bodega.style.display = "flex";
     }
-    terugKnop.onclick = () => {
+    terugKnop1.onclick = () => {
+        theetuin.style.display = "none";
+        bodega.style.display = "none";
+        home.style.display = "flex";
+    }
+    terugKnop2.onclick = () => {
         theetuin.style.display = "none";
         bodega.style.display = "none";
         home.style.display = "flex";
