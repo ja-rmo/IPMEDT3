61f6e5f39f3d8a889b2d9fb97ab6ef144e3747dc
Jarmo <36207725+ja-rmo@users.noreply.github.com>
2022-01-26 15:40:09 +0100

    animations added

--- javascript/startmenu.js
+++ javascript/startmenu.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
     const theetuinKnop = document.getElementById("js--theetuinKnop");
     const bodegaKnop = document.getElementById("js--bodegaKnop");
     const terugKnop1 = document.getElementById("js--terugKnop1");
@@ -6,23 +6,43 @@ window.onload = function() {
     const theetuin = document.getElementById("js--theetuin");
     const bodega = document.getElementById("js--bodega");
     const home = document.getElementById("js--home");
+    const body = document.getElementById("js--body");
 
-    theetuinKnop.onclick = () => {
-        home.style.display = "none";
-        theetuin.style.display = "flex";
-    }
-    bodegaKnop.onclick = () => {
-        home.style.display = "none";
-        bodega.style.display = "flex";
-    }
-    terugKnop1.onclick = () => {
-        theetuin.style.display = "none";
-        bodega.style.display = "none";
-        home.style.display = "flex";
-    }
-    terugKnop2.onclick = () => {
-        theetuin.style.display = "none";
-        bodega.style.display = "none";
-        home.style.display = "flex";
-    }
+    theetuinKnop.addEventListener("click", function () {
+        fadeIn(home, theetuin);
+        body.style.backgroundSize = "100vw 100vh";
+        body.style.backgroundPosition = 0;
+        // body.style.backgroundImage = null;
+        body.style.backgroundImage = "repeating-radial-gradient(circle at 0 0, transparent 0, #f5f5f5 23px), repeating-linear-gradient(#edebe755, #edebe7)";
+    });
+    bodegaKnop.addEventListener("click", function () {
+        fadeIn(home, bodega);
+        body.style.backgroundSize = "36px 36px"
+        body.style.backgroundPosition = 0
+        body.style.backgroundImage = "linear-gradient(135deg, #edebe755 25%, transparent 25%) -18px 0/ 36px 36px, linear-gradient(225deg, #edebe7 25%, transparent 25%) -18px 0/ 36px 36px, linear-gradient(315deg, #edebe755 25%, transparent 25%) 0px 0/ 36px 36px, linear-gradient(45deg, #edebe7 25%, #f5f5f5 25%) 0px 0/ 36px 36px;";
+    });
+    terugKnop1.addEventListener("click", function () {
+        fadeIn(theetuin, home);
+        body.style.backgroundSize = "20px 35px"
+        body.style.backgroundPosition = "0 0, 0 0, 10px 18px, 10px 18px, 0 0, 10px 18px";
+        body.style.backgroundImage = null;
+        body.style.backgroundImage = "linear-gradient(30deg, #edebe7 12%, transparent 12.5%, transparent 87%, #edebe7 87.5%, #edebe7), linear-gradient(150deg, #edebe7 12%, transparent 12.5%, transparent 87%, #edebe7 87.5%, #edebe7), linear-gradient(30deg, #edebe7 12%, transparent 12.5%, transparent 87%, #edebe7 87.5%, #edebe7), linear-gradient(150deg, #edebe7 12%, transparent 12.5%, transparent 87%, #edebe7 87.5%, #edebe7), linear-gradient(60deg, #edebe777 25%, transparent 25.5%, transparent 75%, #edebe777 75%, #edebe777), linear-gradient(60deg, #edebe777 25%, transparent 25.5%, transparent 75%, #edebe777 75%, #edebe777);"
+    });
+    terugKnop2.addEventListener("click", function () {
+        fadeIn(bodega, home)
+        body.style.backgroundSize = "20px 35px"
+        body.style.backgroundPosition = "0 0, 0 0, 10px 18px, 10px 18px, 0 0, 10px 18px";
+        body.style.backgroundImage = null;
+        body.style.backgroundImage = "linear-gradient(30deg, #edebe7 12%, transparent 12.5%, transparent 87%, #edebe7 87.5%, #edebe7), linear-gradient(150deg, #edebe7 12%, transparent 12.5%, transparent 87%, #edebe7 87.5%, #edebe7), linear-gradient(30deg, #edebe7 12%, transparent 12.5%, transparent 87%, #edebe7 87.5%, #edebe7), linear-gradient(150deg, #edebe7 12%, transparent 12.5%, transparent 87%, #edebe7 87.5%, #edebe7), linear-gradient(60deg, #edebe777 25%, transparent 25.5%, transparent 75%, #edebe777 75%, #edebe777), linear-gradient(60deg, #edebe777 25%, transparent 25.5%, transparent 75%, #edebe777 75%, #edebe777);"
+    });
+}
+
+function fadeIn(first, second) {
+    first.style.opacity = 0;
+    first.style.transition = "opacity 0.3s";
+    setTimeout(() => {
+        first.style.display = "none"; second.style.display = "flex";
+        second.style.opacity = 1;
+        second.style.transition = "opacity 0.5s";
+    }, 300);
 }
